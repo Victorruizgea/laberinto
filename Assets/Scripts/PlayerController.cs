07102bd8b9d5a39f1236b039d811b7a361baa15e
Victor Ruiz Gea <victorruizgea@gmail.com>
2024-05-18 19:18:55 +0200

    laberinto terminado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,58 +11,84 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
     private int contador;
 
-	void Start() 
+    public GameObject[] laberintos;
+    private int niveles;
+    void Start() 
 	{
 		rb = GetComponent<Rigidbody>();
-      //  contador = 0;
-       // SetCountText();
-       // winText.text = "";
-	}
+        contador = 0;
+        niveles = 0;
+        SetCountText();
+        for (int i = 0; i < laberintos.Length; i++)
+        {
+            laberintos[i].SetActive(i == 0);
+        }
+
+        winText.text = laberintos[niveles].name;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // float posH = Input.GetAxis("Horizontal");
-        // float posV = Input.GetAxis("Vertical");
-        Vector3 mov = Input.acceleration;
-        mov = Quaternion.Euler(90, 0, 0) * mov;
-       // Vector3 movimiento = new Vector3(posH, 0.0f, posV);
+        float posH = Input.GetAxis("Horizontal");
+        float posV = Input.GetAxis("Vertical");
 
-        rb.AddForce(mov * velocidad);
+        Vector3 movimiento = new Vector3(posH, 0.0f, posV);
 
-    }
+        rb.AddForce(movimiento * velocidad);
 
-    /*
-    void OnTriggerEnter(Collider other)
-    {
-            if (other.gameObject.CompareTag("mono"))
-            {
-                other.gameObject.SetActive(false);
-                contador = contador + 1;
-                SetCountText();
-            }
+        if (rb.position.y < -10f)
+        {
+            winText.text = "Has perdido!! Inténtalo de nuevo :(";
+            Invoke("QuitGame", 1f);
+        }
 
     }
 
     void SetCountText()
     {
-            countText.text = "Contador: " + contador.ToString();
-            if (contador >= 4)
-            {
-                winText.text = "Ganaste!!";
-            }
+        countText.text = "Contador: " + contador.ToString();
     }
 
-    private void OnTriggerExit(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-		if (other.gameObject.CompareTag("tablero"))
-		{
-			winText.text = "Perdiste?!! :(";
-			Invoke("QuitGame", 1f);
-		}
-	}
+        if (other.gameObject.CompareTag("diamanteAmarillo"))
+        {
+            other.gameObject.SetActive(false);
+            contador = contador + 5;
+        }
+        else if (other.gameObject.CompareTag("diamanteNegro"))
+        {
+            other.gameObject.SetActive(false);
+            contador = contador + 30;
+        }
+        else if (other.gameObject.CompareTag("diamanteVerde"))
+        {
+            other.gameObject.SetActive(false);
+            contador = contador + 15;
 
-	void QuitGame()
+        }else if (other.gameObject.CompareTag("salida"))
+        {
+            if (niveles == laberintos.Length - 1)
+            {
+                winText.text = "¡Has ganado!";
+                Invoke("QuitGame", 1f);
+            }
+            else
+            {
+                laberintos[niveles].SetActive(false);
+                niveles++;
+                laberintos[niveles].SetActive(true);
+                winText.text = laberintos[niveles].name;
+                rb.position = new Vector3(0, 0.5f, 0);
+                rb.velocity = Vector3.zero;
+            }
+        }
+        SetCountText();
+        Debug.Log(other.gameObject.tag);
+    }
+ 
+    void QuitGame()
 	{
 		#if UNITY_EDITOR
 		    UnityEditor.EditorApplication.isPlaying = false;
@@ -72,6 +98,7 @@ public class PlayerController : MonoBehaviour {
 		    Application.Quit();
 		#endif
 	}
-    */
+
+
 
 }
